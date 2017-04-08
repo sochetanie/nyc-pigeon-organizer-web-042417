048de16656530943d745803920c76031ab944d51
Alla Gorik <allagorik2901@gmail.com>
2017-04-08 20:01:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+  data.each do |key,value|
+    value.each do |list,name|
+      name.each do |pigeon|
+        result[pigeon] ||= {}
+        result[pigeon][key] ||= []
+        result[pigeon][key] << list.to_s
+      end
+    end
+  end
+  result
 end
